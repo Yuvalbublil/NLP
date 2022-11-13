731a6daaa3406d6159d7bcfe70a05eeaecb9682b
Yuvalbublil <47517044+Yuvalbublil@users.noreply.github.com>
2022-11-13 23:37:51 +0200

    Ex:1 added pickle to shorten the times

--- Ex1/main.py
+++ Ex1/main.py
@@ -1,16 +1,22 @@
 import abc
+import datetime
 import pickle as pkl
+
+import numpy as np
 import spacy
 from datasets import load_dataset
 from spacy.attrs import LEMMA, IS_ALPHA
 from spacy.tokens import Doc, Token
+
+UNIGRAM_PKL = "unigram.pickle"
+BIGRAM_PKL = "bigram.pickle"
 
 START = 'START'
 COUNTER_KEY = '#'
 ATTR_LIST = [LEMMA, IS_ALPHA]
 
 nlp = spacy.load("en_core_web_sm")
-
+# nlp = spacy.load("en")
 
 class NGram:
     def __init__(self):
@@ -18,16 +24,17 @@ class NGram:
 
     def train(self, dataset):
         for text in dataset['text']:
-            doc = nlp(f"{START} {text}")
-            filtered_doc = Doc(doc.vocab, words=[t.text for t in doc if t.is_alpha])
+
+            doc = nlp(text)
+            filtered_doc = Doc(doc.vocab, words=[START]+[t.lemma_ for t in doc if t.is_alpha])
             for i, token in enumerate(filtered_doc):
                 if token.text == START:
                     continue
                 self._handle_token(doc[i-1], token)
 
     def continue_sentence(self, sentence: str):
-        doc = nlp(f"{START} {sentence}")
-        filtered_doc = Doc(doc.vocab, words=[t.text for t in doc if t.is_alpha])
+        doc = nlp(sentence)
+        filtered_doc = Doc(doc.vocab, words=[START] + [t.lemma_ for t in doc if t.is_alpha])
         return self._continue_sentence(filtered_doc)
 
     @staticmethod
@@ -40,12 +47,16 @@ class NGram:
         pass
 
     @abc.abstractmethod
+    def probability_of_word(self, sentence: Doc, i: int):
+        pass
+
+    @abc.abstractmethod
     def _continue_sentence(self, sentence: Doc):
         pass
 
     @staticmethod
     def _get_lemma(token: Token):
-        return token.lemma_ if token.lemma_ else token.text
+        return token.lemma_ if token.lemma and token.text != START else token.text
 
     @staticmethod
     def _max_value_from_dictionary(dictionary: dict):
@@ -59,15 +70,20 @@ class UniGram(NGram):
     def _continue_sentence(self, sentence: Doc):
         return NGram._max_value_from_dictionary(self.dictionary)
 
+    def probability_of_word(self, sentence: Doc, i: int):
+        this_token = sentence[i]
+        prob = self.dictionary.get(NGram._get_lemma(this_token), 0) / self.dictionary.get(COUNTER_KEY, 1)
+        return np.log(prob)
+
 
 class BiGram(NGram):
     def _handle_token(self, prev: Token, token: Token):
-        first_word_dict = self.dictionary.get(NGram._get_lemma(token), {COUNTER_KEY: 0})
-        NGram._update_dictionary(first_word_dict, NGram._get_lemma(prev))
-        self.dictionary[NGram._get_lemma(token)] = first_word_dict
+        first_word_dict = self.dictionary.get(NGram._get_lemma(prev), {COUNTER_KEY: 0})
+        NGram._update_dictionary(first_word_dict, NGram._get_lemma(token))
+        self.dictionary[NGram._get_lemma(prev)] = first_word_dict
 
     def _continue_sentence(self, sentence: Doc):
-        last_token = sentence[len(sentence)-1]
+        last_token = sentence[len(sentence) - 1]
         return NGram._max_value_from_dictionary(self.dictionary[NGram._get_lemma(last_token)])
 
 def train_all():
@@ -78,9 +94,9 @@ def train_all():
 
     unigram.train(dataset)
     bigram.train(dataset)
-    with open("unigram.pkl", "wb") as f:
+    with open(UNIGRAM_PKL, "wb") as f:
         pkl.dump(unigram, f)
-    with open("bigram.pkl", "wb") as f:
+    with open(BIGRAM_PKL, "wb") as f:
         pkl.dump(bigram, f)
     return unigram, bigram
 
@@ -88,9 +104,9 @@ def load_all():
     unigram = UniGram()
     bigram = BiGram()
 
-    with open("unigram.pkl", "rb") as f:
+    with open(UNIGRAM_PKL, "rb") as f:
         unigram = pkl.load(f)
-    with open("bigram.pkl", "rb") as f:
+    with open(BIGRAM_PKL, "rb") as f:
         bigram = pkl.load(f)
     return unigram, bigram
 def main():
@@ -98,7 +114,24 @@ def main():
     print(f'unigram_{unigram.continue_sentence("I have a house in")}_')
     print(f'bigram_{bigram.continue_sentence("I have a house in")}_')
 
+    BRAD_PIT = "Brad Pitt was born in Oklahoma"
+    ACTOR = "The actor was born in USA"
+    print(f'\nprobability for brad pit with bigram is {bigram.probability_of_sentence(BRAD_PIT)}')
+    print(f'probability for Actor with bigram is {bigram.probability_of_sentence(ACTOR)}')
+    print(f'the preplexity with bigram is: {bigram.perplexity([BRAD_PIT, ACTOR])}')
+
+    # print(f'\nprobability for brad pit with unigram is {unigram.probability_of_sentence(BRAD_PIT)}')
+    # print(f'probability for Actor with unigram is {unigram.probability_of_sentence(ACTOR)}')
+    # print(f'the preplexity with unigram is: {bigram.perplexity([BRAD_PIT, ACTOR])}')
+
+    linear_inter = Linear_interpolation(unigram, bigram)
+    print(f'\nprobability for brad pit with linear is {linear_inter.probability_of_sentence(BRAD_PIT)}')
+    print(f'probability for Actor with linear is {linear_inter.probability_of_sentence(ACTOR)}')
+    print(f'the preplexity with linear is: {linear_inter.perplexity([BRAD_PIT, ACTOR])}')
+
 
 if __name__ == "__main__":
-    # train_all()
+    start_time = datetime.datetime.now()
+    train_all()
+    print(f'training time: {datetime.datetime.now()-start_time}\n')
     main()
