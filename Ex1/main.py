d30d3bf19689b99c123e902e3ba6369438083395
Yuvalbublil <47517044+Yuvalbublil@users.noreply.github.com>
2022-11-13 23:37:54 +0200

    Ex1: Done and tested

--- Ex1/main.py
+++ Ex1/main.py
@@ -30,12 +30,33 @@ class NGram:
             for i, token in enumerate(filtered_doc):
                 if token.text == START:
                     continue
-                self._handle_token(doc[i-1], token)
+                self._handle_token(filtered_doc[i - 1], token)
 
     def continue_sentence(self, sentence: str):
         doc = nlp(sentence)
         filtered_doc = Doc(doc.vocab, words=[START] + [t.lemma_ for t in doc if t.is_alpha])
         return self._continue_sentence(filtered_doc)
+
+    def probability_of_sentence(self, sentence: str):
+        doc = nlp(sentence)
+        filtered_doc = Doc(doc.vocab, words=[START] + [t.lemma_ for t in doc if t.is_alpha])
+        p = 0
+        for i, word in enumerate(filtered_doc):
+            if i == 0:
+                continue
+            p += self.probability_of_word(filtered_doc, i)
+        return p
+
+    def perplexity(self, sentences: list):
+        token_set = []
+        probability = 0
+        for i, sentence in enumerate(sentences):
+            probability += self.probability_of_sentence(sentence) * np.log2(np.e)
+            token_set += [t.lemma_ for t in nlp(sentence) if t.is_alpha]
+
+        M = len(token_set)
+        l = probability / M
+        return np.power(2, -l)
 
     @staticmethod
     def _update_dictionary(dictionary: dict, key: str, default=0):
@@ -86,6 +107,28 @@ class BiGram(NGram):
         last_token = sentence[len(sentence) - 1]
         return NGram._max_value_from_dictionary(self.dictionary[NGram._get_lemma(last_token)])
 
+    def probability_of_word(self, sentence: Doc, i: int):
+        last_token = NGram._get_lemma(sentence[i - 1])
+        this_token = NGram._get_lemma(sentence[i])
+        last_dictionary = self.dictionary.get(last_token, 0)
+        prob = last_dictionary.get(this_token, 0) / last_dictionary.get(COUNTER_KEY, 1)
+        return -np.inf if prob == 0 else np.log(prob)
+
+
+class Linear_interpolation(NGram):
+    gamma_unigram = 1.0 / 3
+    gamma_bigram = 1.0 - gamma_unigram
+
+    def __init__(self, unigram: UniGram, bigram: BiGram):
+        self.unigram = unigram
+        self.bigram = bigram
+
+    def probability_of_word(self, sentence: Doc, i: int):
+        uni_prob = self.unigram.probability_of_word(sentence, i)
+        bi_prob = self.bigram.probability_of_word(sentence, i)
+        return np.log(self.gamma_unigram * np.exp(uni_prob) + self.gamma_bigram * np.exp(bi_prob))
+
+
 def train_all():
     dataset = load_dataset('wikitext', 'wikitext-2-raw-v1', split="train")
 
@@ -100,6 +143,7 @@ def train_all():
         pkl.dump(bigram, f)
     return unigram, bigram
 
+
 def load_all():
     unigram = UniGram()
     bigram = BiGram()
@@ -109,10 +153,12 @@ def load_all():
     with open(BIGRAM_PKL, "rb") as f:
         bigram = pkl.load(f)
     return unigram, bigram
+
+
 def main():
     unigram, bigram = load_all()
-    print(f'unigram_{unigram.continue_sentence("I have a house in")}_')
-    print(f'bigram_{bigram.continue_sentence("I have a house in")}_')
+    print(f'unigram: {unigram.continue_sentence("I have a house in")}')
+    print(f'bigram: {bigram.continue_sentence("I have a house in")}')
 
     BRAD_PIT = "Brad Pitt was born in Oklahoma"
     ACTOR = "The actor was born in USA"
